95066a3265dbd8a6f61bfed0fc52021aa4eaed14
Sqwado <mateo.luque@ynov.com>
2023-10-05 12:26:54 +0200

    docker link

--- page/verif.php
+++ page/verif.php
@@ -5,13 +5,19 @@ session_start();
 // propriété pour l'ouverture de la db
 class Database
 {
-    public function __construct(
-        private string $host,
-        private string $port,
-        private string $name,
-        private string $user,
-        private string $password
-    ) {
+    private string $host;
+    private string $port;
+    private string $name;
+    private string $user;
+    private string $password;
+
+    public function __construct($host, $port, $name, $user, $password)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->name = $name;
+        $this->user = $user;
+        $this->password = $password;
     }
 
     //ouverture de la db avec les paramètres attribués
@@ -32,7 +38,7 @@ class Database
 try {
     $database = new Database("176.31.132.185", "3306", "kcdxbd_sqwadowe_db", "kcdxbd_sqwadowe_db", "_Bo%i0j4M!aY76U-");
 
-$conn = $database->getConnection();
+    $conn = $database->getConnection();
 } catch (PDOException $e) {
     echo "Erreur de base de données : " . $e->getMessage();
 }
@@ -48,7 +54,7 @@ $result = $conn->query($sql);
 if ($result->rowCount() == 1) {
 
     $_SESSION['user_id'] = $name;
-    
+
     header('Location: ../page/accueil.php');
     exit();
 } else {
